a607409be77d2b029076b54685bc3bc39cf9afd5
ajunake <kees.vincent.ajunan@oocl.com>
2021-08-05 19:15:06 +0800

    [MarsRover][Kevin]feat: add should_return_LocationX_0_LocationY_0_Direction_S_when_execute_command_given_LocationX_0_LocationY_0_Direction_W_Command_L() test case

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -123,4 +123,19 @@ class DemoTest {
         Assertions.assertEquals(expectedRoverStatus.getLocationY(), marsRover.getRoverStatus().getLocationY());
         Assertions.assertEquals(expectedRoverStatus.getDirection(), marsRover.getRoverStatus().getDirection());
     }
+
+    @Test
+    void  should_return_LocationX_0_LocationY_0_Direction_S_when_execute_command_given_LocationX_0_LocationY_0_Direction_W_Command_L() {
+        //given
+        Marsrover marsRover = new Marsrover(new RoverStatus(0,0,"W"));
+        RoverStatus expectedRoverStatus = new RoverStatus(0,0,"S");
+
+        //when
+        Marsrover.executeCommand("L");
+
+        //then
+        Assertions.assertEquals(expectedRoverStatus.getLocationX(), marsRover.getRoverStatus().getLocationX());
+        Assertions.assertEquals(expectedRoverStatus.getLocationY(), marsRover.getRoverStatus().getLocationY());
+        Assertions.assertEquals(expectedRoverStatus.getDirection(), marsRover.getRoverStatus().getDirection());
+    }
 }
